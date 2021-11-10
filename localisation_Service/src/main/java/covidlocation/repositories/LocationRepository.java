98b32cf1e9d256806f7ada3ff3d7bdf096d03e93
imadlaouani <imadlaouani47@gmail.com>
2021-11-10 21:31:53 +0100

    discovery service location

--- localisation_Service/src/main/java/covidlocation/repositories/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package covidlocation.repositories;
-
-import covidlocation.models.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location,Long> {
-}
