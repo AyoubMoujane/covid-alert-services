bf4d6bb274e0904f184bf21e133f083e7e90409e
imadlaouani <imadlaouani47@gmail.com>
2021-11-21 10:26:36 +0100

    store locations

--- kafka_service/src/main/java/kafkapackage/controllers/KafkaController.java
+++ kafka_service/src/main/java/kafkapackage/controllers/KafkaController.java
@@ -1,36 +1,49 @@
 package kafkapackage.controllers;
 
-import kafkapackage.services.ProducerClass;
+import kafkapackage.models.Location;
+import kafkapackage.services.Producer;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Set;
 
 
 @RestController
 @RequestMapping("kafka")
 public class KafkaController {
 
-    private final ProducerClass producer;
+    private final Producer producer;
 
-    public KafkaController(ProducerClass producer) {
+    @Autowired
+    public KafkaController(Producer producer) {
         this.producer = producer;
     }
-
-
-    @PostMapping(value = "/publish")
-    public String sendMessage (@RequestBody String message){
+    /*
+    @PostMapping("/publish")
+    public void messageToTopic(@RequestParam("message") String message){
         this.producer.sendMessage(message);
-        return "Published succesfully";
 
     }
 
-    @Bean
-    public NewTopic adviceTopic(){
-        return new NewTopic("user",3,(short)1);
+     */
+
+
+    @PostMapping
+    @RequestMapping("/locations")
+    public void sendMessageToKafkaTopic(@RequestBody final Location location) {
+        System.out.println(location);
+        try {
+            this.producer.saveCreateLocationLog(location);
+        }
+        catch (Exception exception) {
+            System.out.println("Error in getNearUser");
+            System.out.println(exception.getMessage());
+            throw exception;
+
+        }
     }
 
 
---
+++ kafka_service/src/main/java/kafkapackage/models/Location.java
@@ -0,0 +1,64 @@
+package kafkapackage.models;
+
+
+import java.sql.Timestamp;
+
+
+
+public class Location {
+
+    private long location_id;
+    private long latitude;
+    private long longitude;
+    private Timestamp location_date;
+
+
+    public Location(long location_id, long latitude, long longitude, Timestamp location_date) {
+        this.location_id = location_id;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.location_date = location_date;
+    }
+
+    public long getLocation_id() {
+        return location_id;
+    }
+
+    public void setLocation_id(long location_id) {
+        this.location_id = location_id;
+    }
+
+    public long getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(long latitude) {
+        this.latitude = latitude;
+    }
+
+    public long getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(long longitude) {
+        this.longitude = longitude;
+    }
+
+    public Timestamp getLocation_date() {
+        return location_date;
+    }
+
+    public void setLocation_date(Timestamp location_date) {
+        this.location_date = location_date;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "location_id=" + location_id +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", location_date=" + location_date +
+                '}';
+    }
+}
