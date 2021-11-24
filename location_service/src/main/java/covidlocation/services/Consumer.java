c4fe6583125c3dea043f34654fbe4438a112915a
Moujane Ayoub <moujane.ayoub@hotmail.com>
2021-11-24 21:22:23 +0100

    testing new value

--- location_service/src/main/java/covidlocation/services/Consumer.java
+++ location_service/src/main/java/covidlocation/services/Consumer.java
@@ -2,6 +2,7 @@ package covidlocation.services;
 
 import covidlocation.models.Location;
 import covidlocation.models.PositiveUser;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +12,11 @@ public class Consumer {
     private static final String TOPIC = "PositiveUsersTopic";
 
     @KafkaListener(topics = TOPIC, groupId = "group_id")
-    public void consume(Object positiveUser)
+    public void consume(ConsumerRecord positiveUser)
     {
         System.out.println("Positive user message consumed -> "+ positiveUser);
         try {
-            PositiveUser positiveUser1 = (PositiveUser) positiveUser;
+            PositiveUser positiveUser1 = (PositiveUser) positiveUser.value();
             System.out.println("Casted received message" + positiveUser1);
 
         } catch(Exception e) {
