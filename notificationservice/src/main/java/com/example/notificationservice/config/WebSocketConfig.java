83171802f6f38d8b136ee99c61d5ca88eeb93ea0
Moujane Ayoub <moujane.ayoub@hotmail.com>
2021-11-25 14:46:58 +0100

    started implementing user registration for location

--- notificationservice/src/main/java/com/example/notificationservice/config/WebSocketConfig.java
+++ notificationservice/src/main/java/com/example/notificationservice/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.example.notificationservice.config;
 
+import com.example.notificationservice.UserHandshakeHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -19,7 +20,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // TODO: add to properties
-        registry.addEndpoint("/our-websocket").setAllowedOrigins("http://localhost:3000").withSockJS();
+        registry.addEndpoint("/our-websocket").setAllowedOrigins("http://localhost:3000")
+                .setHandshakeHandler(new UserHandshakeHandler())
+                .withSockJS();
     }
 
 }
