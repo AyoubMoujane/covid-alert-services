4fc8243ad95c2d4d017ccd512ce3580f9f1b4311
Pierre34170 <perr1.pierre34@gmail.com>
2021-11-25 08:40:31 +0100

    add changes in kafka_service

--- kafka_service/src/main/java/kafkapackage/services/Consumer.java
+++ kafka_service/src/main/java/kafkapackage/services/Consumer.java
@@ -4,7 +4,14 @@ import kafkapackage.models.Location;
 import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class Consumer {
@@ -18,20 +25,26 @@ public class Consumer {
 
      */
 
+    private List<Location> locations = new ArrayList<>();
+
+
     @KafkaListener(topics = "location_topic",
             groupId = "group_id")
-    public void consume(Location location)
+    public void consume(Location location, @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition, @Header(KafkaHeaders.OFFSET) int offset)
     {
         System.out.println("Location consumed -> "+ location);
-//        System.out.println(process().);
+        System.out.println("From partition : "+ partition);
     }
 
-//    @Bean
-//    public java.util.function.Consumer<KStream<String, String>> process() {
-//
-//        return input ->
-//                input.foreach((key, value) -> {
-//                    System.out.println("Key: " + key + " Value: " + value);
-//                });
-//    }
+    @KafkaListener(topicPartitions =
+            {@TopicPartition(topic = "location_topic",
+                    partitionOffsets = @PartitionOffset(partition="0", initialOffset = "0"))
+            })
+    public void consumeAllLocation(Location location)
+    {
+        locations.add(location);
+        System.out.println(locations);
+
+    }
+
 }
